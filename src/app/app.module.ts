e8ef20995afc0644a88d3f3871bac8fbe0d97c62
Jesus Quevedo <jesus.quevedo@morningstar.com>
2021-10-31 21:38:07 -0400

    adding posts, header and comments components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts-list/posts-list.component';
+import { PostItemComponent } from './posts-list/post-item/post-item.component';
+import { PostDetailComponent } from './posts-list/post-detail/post-detail.component';
+import { CommentsComponent } from './posts-list/post-detail/comments/comments.component';
+import { CommentsEditorComponent } from './posts-list/post-detail/comments-editor/comments-editor.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    PostItemComponent,
+    PostDetailComponent,
+    CommentsComponent,
+    CommentsEditorComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts-list/post-detail/comments-editor/comments-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-editor',
+  templateUrl: './comments-editor.component.html',
+  styleUrls: ['./comments-editor.component.scss']
+})
+export class CommentsEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
