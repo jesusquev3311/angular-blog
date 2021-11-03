015754f3d3c4df49f7756ef64382aa26ca9d647e
Jesus Quevedo <jesus.quevedo@morningstar.com>
2021-11-03 13:47:45 -0400

    updating delete service

--- src/app/post-manager/post-manager.component.ts
+++ src/app/post-manager/post-manager.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { PostsService } from '../services/posts.service';
-import { Post } from '../shared/post/post.model';
+import { Component, OnInit } from "@angular/core";
+import { PostsService } from "../services/posts.service";
+import { Post } from "../shared/post/post.model";
 
 @Component({
-  selector: 'app-post-manager',
-  templateUrl: './post-manager.component.html',
-  styleUrls: ['./post-manager.component.scss']
+  selector: "app-post-manager",
+  templateUrl: "./post-manager.component.html",
+  styleUrls: ["./post-manager.component.scss"],
 })
 export class PostManagerComponent implements OnInit {
   posts: Post[] = [];
@@ -13,23 +13,26 @@ export class PostManagerComponent implements OnInit {
   constructor(private PostsService: PostsService) {}
 
   ngOnInit(): void {
-    if(this.posts.length <= 0){
+    if (this.posts.length <= 0) {
       this.dataProvider();
     }
   }
 
   dataProvider() {
-    return this.PostsService.getAll().subscribe((posts) => (this.posts = posts));
+    return this.PostsService.getAll().subscribe(
+      (posts) => (this.posts = posts)
+    );
   }
 
-  deleteProvider(event){
+  deleteProvider(event) {
     const { index, id } = event;
-      /* 
+    /* 
         As the API doesn't really delete the element, 
         I've simulated the removel from the posts array
       */
-      if(this.posts.splice(index, 1)){
-        this.PostsService.deleteById(id)
-      }
+    this.PostsService.deleteById(id).subscribe((resp) => {
+      console.log(resp);
+      this.posts.splice(index, 1);
+    });
   }
 }
